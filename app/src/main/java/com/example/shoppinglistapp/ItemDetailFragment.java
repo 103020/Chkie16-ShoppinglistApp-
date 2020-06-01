3bc0d33dbca2fc89e31937aa973b8d2b16a69623
Christian Kierkegaard <kierkegaardchristian@gmail.com>
2020-06-01 22:12:03 +0200

    Update ItemDetailFragment.java

--- app/src/main/java/com/example/shoppinglistapp/ItemDetailFragment.java
+++ app/src/main/java/com/example/shoppinglistapp/ItemDetailFragment.java
@@ -48,10 +48,6 @@ public class ItemDetailFragment extends Fragment {
     public void onStart() {
         super.onStart();
 
-        // During startup, check if there are arguments passed to the fragment.
-        // onStart is a good place to do this because the layout has already been
-        // applied to the fragment at this point so we can safely call the method
-        // below that sets the article text.
         Bundle args = getArguments();
         if (args != null) {
             // Set article based on argument passed in
